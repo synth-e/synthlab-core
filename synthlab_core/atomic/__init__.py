2ccde2fcd8dc5ac4d50233c5b082e3a6625608c4
tndo <dotrann.1412@gmail.com>
2024-09-07 10:50:45 +0700

    update

--- synthlab_core/atomic/__init__.py
+++ synthlab_core/atomic/__init__.py
@@ -1,6 +1,14 @@
 from .base import AtomicType
 from .image import ImageWrapper, Sketch, DiffusionResponse
 from .mask import MaskWrapper
-from .text import TextualPrompt
+from .text import TextualPrompt, Identity
 from .box import BBoxListWrapper, BBoxWrapper
 from .index import IndexedFile
+
+from synthlab_core.registry import register, ClassType
+
+for e in [AtomicType, ImageWrapper, Sketch, 
+     DiffusionResponse, MaskWrapper, 
+     TextualPrompt, BBoxListWrapper, 
+     BBoxWrapper, IndexedFile, Identity]: 
+    register(ClassType.DATA_TYPE, e)
